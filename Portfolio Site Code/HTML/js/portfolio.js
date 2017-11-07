62782f40a304c0aa1f4316fa65d9ad45622d38a4
Cyrus Shahrivar <cyrus.shahrivar@gmail.com>
2017-11-07 07:49:06 -0500

    add new mobile nav to portfolio page

--- Portfolio Site Code/HTML/js/portfolio.js
+++ Portfolio Site Code/HTML/js/portfolio.js
@@ -44,14 +44,52 @@ $(document).ready(function () {
         var $body = $('body');
         $body.html(compiledHtml);
 
-        $('.header__open-button').click(function() {
-            $('.header__nav-bar--mobile').slideDown();
+        // Mobile Header Animations
+        var $mobileHeaderOpenButton = $('.header__open-button');
+        var $mobileHeaderCloseButton = $('.header__close-button');
+        var $mobileNav = $('.header__nav-bar--mobile');
+        var $mobileBackgroundMask = $('.header__background-mask');
+        var $mobileNavListItems = $('.header__nav-bar--mobile li');
+
+        $mobileHeaderOpenButton.click(function() {
+            $mobileNav.addClass('opening');
+            // slide open header
+            $mobileNav.slideDown('slow', function () {
+                // fade in image
+                $mobileBackgroundMask.css('opacity', 0);
+                $mobileBackgroundMask.on('transitionend', function () {
+                    // slide down nav items
+                    $mobileNavListItems.css('opacity', 1);
+                    $mobileNavListItems.on('transitionend', function () {
+                        $mobileNav.removeClass('opening');
+                        $mobileNavListItems.off('transitionend');
+                    });
+                    $mobileBackgroundMask.off('transitionend')
+                })
+            });
         });
 
-        $('.header__close-button').click(function() {
-            $('.header__nav-bar--mobile').slideUp();
+        $mobileHeaderCloseButton.click(function() {
+            $mobileNav.addClass('closing');
+            // slide up nav items
+            $mobileNavListItems.css('opacity', 0);
+            $mobileBackgroundMask.on('transitionend', function () {
+                // slide close header
+                $mobileNav.slideUp('slow', function () {
+                    // do something?
+                });
+                $mobileBackgroundMask.off('transitionend');
+            });
+
+            $mobileNavListItems.on('transitionend', function () {
+                $mobileNav.removeClass('closing');
+                // fade out image
+                $mobileBackgroundMask.css('opacity', 1);
+                $mobileNavListItems.off('transitionend');
+            });
+
         });
 
-        $('.header__nav-bar--mobile').hide();
+        $mobileNav.hide();
     });
 })
