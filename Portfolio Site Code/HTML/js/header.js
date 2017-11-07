285eb27cf4daa92b98a01a7a007071e99ff1fbfd
Cyrus Shahrivar <cyrus.shahrivar@gmail.com>
2017-11-07 07:01:50 -0500

    cache mobile menu animation selectors

--- Portfolio Site Code/HTML/js/header.js
+++ Portfolio Site Code/HTML/js/header.js
@@ -1,34 +1,46 @@
 $(document).ready(function() {
-    $('.header__open-button').click(function() {
-        $('.header__nav-bar--mobile').addClass('opening');
-        // slide open header
-        $('.header__nav-bar--mobile').slideDown('slow', function () {
-            // fade in image
-            $('.header__background-mask').css('opacity', 0);
-            // slide down nav items
-            $('.header__nav-bar--mobile li').css('opacity', 1);
+    var $mobileHeaderOpenButton = $('.header__open-button');
+    var $mobileHeaderCloseButton = $('.header__close-button');
+    var $mobileNav = $('.header__nav-bar--mobile');
+    var $mobileBackgroundMask = $('.header__background-mask');
+    var $mobileNavListItems = $('.header__nav-bar--mobile li');
 
+    $mobileHeaderOpenButton.click(function() {
+        $mobileNav.addClass('opening');
+        // slide open header
+        $mobileNav.slideDown('slow', function () {
+            // fade in image
+            $mobileBackgroundMask.css('opacity', 0);
+            $mobileBackgroundMask.on('transitionend', function () {
+                // slide down nav items
+                $mobileNavListItems.css('opacity', 1);
+                $mobileBackgroundMask.off('transitionend')
+            })
         });
 
-        $('.header__nav-bar--mobile').removeClass('opening');
+        $mobileNav.removeClass('opening');
     });
 
-    $('.header__close-button').click(function() {
-        $('.header__nav-bar--mobile').addClass('closing');
+    $mobileHeaderCloseButton.click(function() {
+        $mobileNav.addClass('closing');
         // slide up nav items
-        $('.header__nav-bar--mobile li').css('opacity', 0);
-        // fade out image
-        $('.header__background-mask').css('opacity', 1);
-        // slide close header
-        $('.header__background-mask').on('transitionend', function (params) {
-            $('.header__nav-bar--mobile').slideUp('slow', function () {
+        $mobileNavListItems.css('opacity', 0);
+        $mobileBackgroundMask.on('transitionend', function () {
+            $mobileNav.slideUp('slow', function () {
                 // do something?
             });
-            $('.header__background-mask').off('transitionend');
+            $mobileBackgroundMask.off('transitionend');
         });
 
-        $('.header__nav-bar--mobile').removeClass('closing');
+        $mobileNavListItems.on('transitionend', function () {
+            // fade out image
+            $mobileBackgroundMask.css('opacity', 1);
+            $mobileNavListItems.off('transitionend');
+        });
+        // slide close header
+
+        $mobileNav.removeClass('closing');
     });
 
-    $('.header__nav-bar--mobile').hide();
+    $mobileNav.hide();
 });
